0a6f8d52ca8ac5aec946c6fc7aa95d11c5947e00
shayanAmarAly <101214599+shayanAmarAly@users.noreply.github.com>
2023-02-02 08:06:05 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+const response = await inquirer.prompt([
+    {
+        name: "total_words",
+        type: "input",
+        message: "Enter a message...",
+    },
+]);
+let wordModi1 = response.total_words.trim(); // remove all extra spaces from the starting of the string.
+console.log(wordModi1);
+if (wordModi1 === "") {
+    console.log("Opp's! You didn't right character");
+}
+else {
+    let wordModi2 = wordModi1.replace(/ +/g, " "); // remove extra spaces from whole string.
+    console.log(wordModi2);
+    let split_WORDS = wordModi2.split(" "); // split the string from single whitespace and returns an array.
+    console.log(`The length is ${split_WORDS}`);
+    let sentenceLength = split_WORDS.length;
+    console.log(`The length is ${sentenceLength}`);
+}
